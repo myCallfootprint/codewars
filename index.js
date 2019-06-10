91caa8a50e5eef427c0c3908deffd743b205d586
myCall <mycallthomas@gmail.com>
2019-06-10 12:51:47 +0100

    added title and output

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+// sort last character
+
 function last(x){
     return x.split(' ').sort((a,b) => a.charCodeAt(a.length -1) - b.charCodeAt(b.length -1))
   }
   
   
-  console.log(last("Mic Whit Conor Sam"));
+  console.log(last("Mic Whit Conor Sam")); // ["Mic", "Sam", "Conor", "Whit"]
   
   
